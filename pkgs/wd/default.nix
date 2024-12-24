00400e8bb555b633f9ebebe3152d195a07236abf
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2024-12-24 10:54:06 +0900

    use wd's default.nix

--- pkgs/wd/default.nix
+++ pkgs/wd/default.nix
@@ -1,23 +1,14 @@
-{ stdenv, system, fetchzip, lib }:
+{ pkgs, fetchzip }:
 
-stdenv.mkDerivation rec {
-  pname = "wd";
-  version = "1.1.0";
-  src = fetchzip {
-    url = "https://github.com/kakkun61/wd/tarball/${version}";
-    sha256 = "sha256-WNwsgSOcYVfXnJ7/DayvlE490Jheum9gCkoEdnGdggM=";
-    extension = "tar.gz";
-  };
-  buildPhase = "make -C linux";
-  installPhase = "make -C linux install out=$out";
-  meta = with lib; {
-    homepage = https://github.com/kakkun61/wd;
-    changelog = "https://github.com/kakkun61/wd/releases/tag/${version}";
-    license = licenses.gpl3;
-    maintainers = [
-      { name = "Kazuki Okamoto (岡本和樹)"; }
-    ];
-    platforms = platforms.linux ++ platforms.darwin;
-    broken = true;
-  };
+let
+  version = "80e0814140c33201adf0e5213c426fb7bd5f47cf";
+  project =
+    fetchzip {
+      url = "https://github.com/kakkun61/wd/tarball/${version}";
+      sha256 = "sha256-usZTBF9pk0IsGdZb6IqEHBEaBX7mnXutykpm9TJHAWk=";
+      extension = "tar.gz";
+    };
+in
+(import "${project}/linux" { inherit pkgs; }).default.overrideAttrs {
+  broken = true;
 }
